0e15c23f0cd6fcb0728db69403e5bb728c416aeb
Andrey Pestunov <andreypestunov@gmail.com>
2023-06-16 02:12:02 +0200

    refactored Result

--- src/Net.Shared.Models/Domain/Result.cs
+++ src/Net.Shared.Models/Domain/Result.cs
@@ -7,53 +7,44 @@ namespace Net.Shared.Models.Domain;
 public sealed record Result<T>
 {
     public T[] Data { get; }
-    public bool IsSuccess { get; }
     public string[] Errors { get; }
-
-    public Result(bool isSuccess)
-    {
-        IsSuccess = isSuccess;
-        Data = Array.Empty<T>();
-        Errors = isSuccess
-            ? Array.Empty<string>()
-            : new[] { "Result unknown error" };
-    }
 
     public Result(T data)
     {
-        IsSuccess = true;
         Data = new[] { data };
         Errors = Array.Empty<string>();
     }
 
     public Result(IEnumerable<T> data)
     {
-        IsSuccess = true;
         Data = data.ToArray();
         Errors = Array.Empty<string>();
     }
-
-    public Result(string[] errors)
-    {
-        IsSuccess = false;
-        Data = Array.Empty<T>();
-        Errors = errors;
-    }
-
-    public Result(string error)
-    {
-        IsSuccess = false;
-        Data = Array.Empty<T>();
-        Errors = new[] { error };
-    }
-
+    
     public Result(Exception exception)
     {
-        IsSuccess = false;
         Data = Array.Empty<T>();
         Errors = exception.InnerException is null
             ? new string[] { exception.Message }
             : new string[] { exception.Message, exception.InnerException.Message };
+    }
+
+    public Result(string error)
+    {
+        Data = Array.Empty<T>();
+        Errors = new[] { error };
+    }
+
+    public Result(IEnumerable<string> errors)
+    {
+        Data = Array.Empty<T>();
+        Errors = errors.ToArray();
+    }
+
+    public Result(IEnumerable<T> data, IEnumerable<string> errors)
+    {
+        Data = data.ToArray();
+        Errors = errors.ToArray();
     }
 
     public string GetError() => string.Join(", ", Errors);
