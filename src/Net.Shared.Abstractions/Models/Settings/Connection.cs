c23461b1dc208239607dcb4b87152a6bc549dbf2
Andrey Pestunov <masterlifting@gmail.com>
2024-01-02 00:31:04 +0100

    feat: upgrade connection settings

--- src/Net.Shared.Abstractions/Models/Settings/Connection.cs
+++ src/Net.Shared.Abstractions/Models/Settings/Connection.cs
@@ -1,10 +1,16 @@
-﻿namespace Net.Shared.Abstractions.Models.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Net.Shared.Abstractions.Models.Settings
 {
     public abstract record Connection
     {
+        [Required]
         public string Host { get; set; } = null!;
+        [Range(1, 65535)]
         public int Port { get; set; }
+        [Required]
         public string User { get; set; } = null!;
+        [Required]
         public string Password { get; set; } = null!;
     }
 }
