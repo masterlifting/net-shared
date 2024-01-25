150311d267d437489f93a5e9c62d9a8f341d8d9d
Andrey Pestunov <masterlifting@gmail.com>
2024-01-25 10:14:18 +0100

    refactor: improve background

---
+++ src/shared-abstractions/Models/Settings/CorrelationSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Net.Shared.Abstractions.Models.Settings;
+
+public sealed record CorrelationSettings
+{
+    public const string SectionName = "Correlation";
+    public Guid Id { get; init; }
+}
---
+++ src/shared/Registrations..cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Net.Shared.Abstractions.Models.Settings;
+
+namespace Net.Shared;
+
+public static class Registrations
+{
+    public static IServiceCollection AddCorrelationSettings(this IServiceCollection services)
+    {
+        services
+            .AddOptions<CorrelationSettings>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration
+                    .GetSection(CorrelationSettings.SectionName)
+                    .Bind(settings);
+            })
+            .ValidateOnStart()
+            .Validate(x => x.Id != Guid.Empty, "Correlation id should not be empty.");
+
+        return services;
+    }
+}
