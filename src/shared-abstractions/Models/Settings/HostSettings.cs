712c5c877e3c160e9bf4d5a51010edfa96303d59
Andrey Pestunov <masterlifting@gmail.com>
2024-01-25 08:23:12 +0100

    feat: improve background

---
+++ src/shared-abstractions/Models/Settings/HostSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Net.Shared.Abstractions.Models.Settings
+{
+    public sealed record HostSettings
+    {
+        public const string SectionName = "Host";
+        public Guid Id { get; init; }
+    }
+}
