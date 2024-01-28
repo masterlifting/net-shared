76e9a91139f3664764320c5bf9c52272d33d9988
Andrey Pestunov <masterlifting@gmail.com>
2024-01-28 15:12:53 +0100

    refactor: improve shared

---
+++ src/shared/Extensions/Expression/ExpressionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Net.Shared.Extensions.Expression;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> Combine<T>(this Expression<Func<T, bool>> firstExpression, Expression<Func<T, bool>> secondExpression)
+    {
+        var invokedExpression = System.Linq.Expressions.Expression.Invoke(secondExpression, firstExpression.Parameters);
+        var combinedExpression = System.Linq.Expressions.Expression.AndAlso(firstExpression.Body, invokedExpression);
+        return System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(combinedExpression, firstExpression.Parameters);
+    }
+}
