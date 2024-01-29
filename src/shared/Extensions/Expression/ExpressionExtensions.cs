28569abe98639c5c284416e83a069c0792635d20
Andrey Pestunov <masterlifting@gmail.com>
2024-01-29 10:55:42 +0100

    refactor: improve expressions

--- src/shared/Extensions/Expression/ExpressionExtensions.cs
+++ src/shared/Extensions/Expression/ExpressionExtensions.cs
@@ -5,10 +5,30 @@ namespace Net.Shared.Extensions.Expression;
 
 public static class ExpressionExtensions
 {
-    public static Expression<Func<T, bool>> Combine<T>(this Expression<Func<T, bool>> firstExpression, Expression<Func<T, bool>> secondExpression)
+    public static Expression<Func<T, bool>> Combine<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
     {
-        var invokedExpression = System.Linq.Expressions.Expression.Invoke(secondExpression, firstExpression.Parameters);
-        var combinedExpression = System.Linq.Expressions.Expression.AndAlso(firstExpression.Body, invokedExpression);
-        return System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(combinedExpression, firstExpression.Parameters);
+        var parameter = System.Linq.Expressions.Expression.Parameter(typeof(T));
+
+        var body1 = ReplaceParameter(expr1.Body, expr1.Parameters[0], parameter);
+        var body2 = ReplaceParameter(expr2.Body, expr2.Parameters[0], parameter);
+
+        var combinedBody = System.Linq.Expressions.Expression.AndAlso(body1, body2);
+
+        return System.Linq.Expressions.Expression.Lambda<Func<T, bool>>(combinedBody, parameter);
+    }
+
+    private static System.Linq.Expressions.Expression ReplaceParameter(
+        System.Linq.Expressions.Expression body,
+        ParameterExpression original,
+        ParameterExpression replacement) =>
+            new ParameterReplacerVisitor(original, replacement).Visit(body);
+
+    private class ParameterReplacerVisitor(ParameterExpression original, ParameterExpression replacement) : ExpressionVisitor
+    {
+        private readonly ParameterExpression _original = original;
+        private readonly ParameterExpression _replacement = replacement;
+
+        protected override System.Linq.Expressions.Expression VisitParameter(ParameterExpression node) =>
+            node == _original ? _replacement : base.VisitParameter(node);
     }
 }
